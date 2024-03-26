89beb852072903c8fb6f831808ec84ad12e931c6
manish <manishsalitra4@gmail.com>
2024-03-26 04:28:48 -0700

    first commit

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,22 @@
+class LoginsController < ApplicationController
+
+
+    def login 
+        @user = User.find_by!(email: login_params[:email])
+        if @user.authenticate(login_params[:password])
+            @token = encode_token(user_id: @user.id)
+            render json: { user: @user, token: @token }, status: :accepted
+        else
+            render json: {message: 'Incorrect password'}, status: :unauthorized
+        end
+
+    end
+
+    private 
+
+    def login_params 
+        params.permit(:email, :password)
+    end
+
+    
+end
